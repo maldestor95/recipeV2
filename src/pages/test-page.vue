f17b7b088aa2149f52276272f19f457cad2b0a99
maldestor95 <maldestor95@gmail.com>
2022-10-14 07:10:18 +0200

    navbard component + vueuse

---
+++ src/pages/test-page.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import VueScroll from './test/vue-scroll.vue';
+</script>
+
+<template>
+    <!-- <VueScroll></VueScroll> -->
+    <div v-for="item in 100">
+        
+        <page-vue>
+            <template #header>
+                recettes
+        </template>
+        Page recettes
+        </page-vue> 
+    </div>
+</template>
