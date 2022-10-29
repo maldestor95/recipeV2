7815950da801618e4380bf556044ddbd676450b0
maldestor95 <maldestor95@gmail.com>
2022-10-29 08:32:35 +0200

    [refactor] recette uses classes

--- src/pages/recipe/recipe-page.vue
+++ src/pages/recipe/recipe-page.vue
@@ -1,15 +1,12 @@
 <script setup lang="ts">
 import recipeBar from '../../../tests/data/baraupetitlegume'
-// import {marked} from 'marked'
 
-import {parseRecipe} from "./recipe"
+import { Recipe} from "./recipe"
 import RecipePageView from './recipe-page-view.vue';
 import RecipePageSearch from './recipe-page-search.vue';
 import RecipePageEdit from './recipe-page-edit.vue';
 
-const recipeName = 'Example de recette'
-const displayRecipe = parseRecipe(recipeBar)
-// const steps = !displayRecipe.steps?"":marked(<string>displayRecipe.steps)
+const displayRecipe = new Recipe(recipeBar)
 </script>
 
 <template>
--- src/pages/recipe/recipe.ts
+++ src/pages/recipe/recipe.ts
@@ -1,40 +1,68 @@
+
 import yaml from 'yaml'
 
 type Ingredient =
-{ ingredient:string,
-    qty: string}
+    {
+        ingredient: string,
+        qty: string
+    }
 
-export type RecipeType ={ 
+export type RecipeType = {
     title: string,
-    link:  string,
-    ingredients:Ingredient[]
-    steps?:string
+    link: string,
+    ingredients: Ingredient[]
+    steps?: string
 }
 
-const convertMarkdownRecipe= function(mdData:string):{err:string|null,data: {yml:string,md:string}} {
+const convertMarkdownRecipe = function (mdData: string): { err: string | null, data: { yml: string, md: string } } {
 
     const startYamlPosition = mdData.indexOf('---');
-    if (startYamlPosition <0) return {err:'no Yaml Data at the beginning of the markdown document',data:{yml:"",md:""}}
-    
+    if (startYamlPosition < 0) return { err: 'no Yaml Data at the beginning of the markdown document', data: { yml: "", md: "" } }
+
     const endYamlPosition = mdData.indexOf('...');
-    if (endYamlPosition<startYamlPosition) return {err:'no Yaml Data at the beginning of the markdown document',data:{yml:"",md:""}}
+    if (endYamlPosition < startYamlPosition) return { err: 'no Yaml Data at the beginning of the markdown document', data: { yml: "", md: "" } }
 
-    const extractYmlPart= mdData.substring(0, endYamlPosition + 3);   
-    const extractMdPart= mdData.substring(endYamlPosition + 3);
+    const extractYmlPart = mdData.substring(0, endYamlPosition + 3);
+    const extractMdPart = mdData.substring(endYamlPosition + 3);
     // console.log(extractMdPart)
-    return {err:null,data:{yml:extractYmlPart,md:extractMdPart}}
+    return { err: null, data: { yml: extractYmlPart, md: extractMdPart } }
 }
 
-export function parseRecipe(recipe:string):Recipe{
-    const {data}=convertMarkdownRecipe(recipe)
-    const dataParsed= yaml.parse(data.yml)
-    const ParsedRecipe:RecipeType= {
-        title: dataParsed.title,
-        link:  dataParsed.link,
-        ingredients:dataParsed.ingredients,
-        steps: data.md
+
+export class Recipe {
+    #recipeDetails: RecipeType
+    #validData: boolean
+
+    constructor(markdownRecipe: string) {
+        const { data, err } = convertMarkdownRecipe(markdownRecipe)
+        const dataParsed = yaml.parse(data.yml)
+        this.#validData = err ? true : false
+        this.#recipeDetails = {
+            title: err ? "" : dataParsed.title,
+            link: err ? "" : dataParsed.link,
+            ingredients: err ? [] : dataParsed.ingredients,
+            steps: err ? "" : data.md
+        }
     }
-    return ParsedRecipe
+
+    get title():string {
+        return this.#recipeDetails.title?"":this.#recipeDetails.title
+    }
+    get link():string {
+        return this.#recipeDetails.link?"":this.#recipeDetails.link
+    }
+    get steps():string {
+        return this.#recipeDetails.steps?"":<string>this.#recipeDetails.steps
+    }
+    /**
+     * return an array of Ingredients
+     * @returns Array of Ingredients
+     */
+    get ingredients():Ingredient[] {
+        return this.#recipeDetails.ingredients
+    }
+    /** @returns true if recipe is valid; false otherwise */
+    get isRecipeValid() { return this.#validData }
+
 }
 
-export default {parseRecipe}
