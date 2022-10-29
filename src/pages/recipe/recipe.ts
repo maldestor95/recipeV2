8567b5c42e8dbf4b31c12ef4f9143a93c6f28017
maldestor95 <maldestor95@gmail.com>
2022-10-29 10:15:50 +0200

    (refactor): move private method recipe

--- src/pages/recipe/recipe.ts
+++ src/pages/recipe/recipe.ts
@@ -14,27 +14,12 @@ export type RecipeType = {
     steps?: string
 }
 
-const convertMarkdownRecipe = function (mdData: string): { err: string | null, data: { yml: string, md: string } } {
-
-    const startYamlPosition = mdData.indexOf('---');
-    if (startYamlPosition < 0) return { err: 'no Yaml Data at the beginning of the markdown document', data: { yml: "", md: "" } }
-
-    const endYamlPosition = mdData.indexOf('...');
-    if (endYamlPosition < startYamlPosition) return { err: 'no Yaml Data at the beginning of the markdown document', data: { yml: "", md: "" } }
-
-    const extractYmlPart = mdData.substring(0, endYamlPosition + 3);
-    const extractMdPart = mdData.substring(endYamlPosition + 3);
-    // console.log(extractMdPart)
-    return { err: null, data: { yml: extractYmlPart, md: extractMdPart } }
-}
-
-
 export class Recipe {
     #recipeDetails: RecipeType
     #validData: boolean
 
     constructor(markdownRecipe: string) {
-        const { data, err } = convertMarkdownRecipe(markdownRecipe)
+        const { data, err } = this.convertMarkdownRecipe(markdownRecipe)
         const dataParsed = yaml.parse(data.yml)
         this.#validData = err ? true : false
         this.#recipeDetails = {
@@ -44,7 +29,19 @@ export class Recipe {
             steps: err ? "" : data.md
         }
     }
+    private convertMarkdownRecipe = function (mdData: string): { err: string | null, data: { yml: string, md: string } } {
 
+        const startYamlPosition = mdData.indexOf('---');
+        if (startYamlPosition < 0) return { err: 'no Yaml Data at the beginning of the markdown document', data: { yml: "", md: "" } }
+
+        const endYamlPosition = mdData.indexOf('...');
+        if (endYamlPosition < startYamlPosition) return { err: 'no Yaml Data at the beginning of the markdown document', data: { yml: "", md: "" } }
+
+        const extractYmlPart = mdData.substring(0, endYamlPosition + 3);
+        const extractMdPart = mdData.substring(endYamlPosition + 3);
+        // console.log(extractMdPart)
+        return { err: null, data: { yml: extractYmlPart, md: extractMdPart } }
+    }
     get title():string {
         return this.#recipeDetails.title?"":this.#recipeDetails.title
     }
